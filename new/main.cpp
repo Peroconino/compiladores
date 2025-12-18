af8a39724afbadb742c74998a02425344738c24e
peroconino 😳 <leandrofontellas@gmail.com>
2025-12-18 14:28:02 -0300

    criando esqueleto inicial

--- new/main.cpp
+++ new/main.cpp
@@ -1,185 +1,146 @@
-#include <bits/stdc++.h>
 #include "main.h"
-using namespace std;
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_CHAR_READ_CODE 2
+typedef struct {
+  char buffer[BUFFER_SIZE];
+  int ini = 0, prox = 0, line = 1, column = 1;
+  bool eof = false, has_lookahead = false;
+  FILE *file;
+} State;
 
-string code, buffer = "";
-int ini = 0, prox = 0, line = 1, column = 1;
+State state;
 
-string PassFileToString() {
-    ifstream file("file.txt");
-    if (!file) throw runtime_error("Error to open file");
-    
-    string fileInString(
-        (istreambuf_iterator<char>(file)),
-        istreambuf_iterator<char>()
-    );
-    return fileInString;
-}
-
-string getToBuffer() {
-    return code.substr(ini, MAX_CHAR_READ_CODE);
+void ReadFile(FILE *input) {
+  fread(state.buffer, sizeof(char), BUFFER_SIZE, input);
 }
 
 bool NotLetterUnderlineOrDigit(char c) {
-    return (!isalpha(c) || !isdigit(c) || c != '_');
+  return (!isalpha(c) || !isdigit(c) || c != '_');
 }
 
 char getNextChar() {
-    char c;
- 
-        c = buffer[ini++];
- 
-        if (c == '\n') {
-            line++;
-            column = 1;
-        } else {
-            column++;
-        }
-  
-    return c;
+  char c;
+  do {
+
+    c = state.buffer[state.ini++];
+
+    if (c == '\n') {
+      state.line++;
+      state.column = 1;
+    } else {
+      state.column++;
+    }
+  } while (isspace(c));
+  return c;
 }
 
-optional<ReturnToken> diagramVoidWithIdentifier() {
-    int state = 0;
-    string lexeme = "";
-    char c;
-
-    while (true) {
-        
-        switch (state) {
-            case 0:
-                c = getNextChar();
-                if (c == 'v') {
-                    lexeme += c;
-                    state = 1;
-                } else return nullopt;
-                break;
-
-            case 1:
-                c = getNextChar();
-                if (c == 'o') {
-                    lexeme += c;
-                    state = 2;
-                } else return nullopt;
-                break;
-
-            case 2:
-                c = getNextChar();
-                if (c == 'i') {
-                    lexeme += c;
-                    state = 3;
-                } else return nullopt;
-                break;
-
-            case 3:
-                c = getNextChar();
-                if (c == 'd') {
-                    lexeme += c;
-                    state = 4;
-                } else if (NotLetterUnderlineOrDigit(c)) {
-
-                    
-                } else return nullopt;
-                break;
-
-            case 4:
-                c = getNextChar();
-                if (isalpha(c) || isdigit(c)) {
-                    lexeme += c;
-                    
-                } else {
-                }
-                break;
-        }
+Token *getNextToken() {
+  int estado = 0;
+  while (true) {
+    switch (estado) {
+    case 0: {
+      char c = getNextChar();
+      if (c == ')') {
+        estado = 1; // x2
+      } else if (c == '(') {
+        estado = 2; // w2
+      } else if (c == '\'') {
+        estado = 3; // v2
+      } else if (c == '*') {
+        estado = 5; // t2
+      } else if (c == '/') {
+        estado = 6; // s3
+      } else if (c == '-') {
+        estado = 7; // r3
+      } else if (c == '+') {
+        estado = 8; // r2
+      } else if (c == '!') {
+        estado = 9; // q10
+      } else if (c == '<') {
+        estado = 10; // q7
+      } else if (c == '>') {
+        estado = 11; // q4
+      } else if (c == '=') {
+        estado = 12; // q2
+      } else if (c == ',') {
+        estado = 13; // y3
+      } else if (c == ';') {
+        estado = 14; // y2
+      } else if (c == ':') {
+        estado = 15; // z2
+      } else if (c == '{') {
+        estado = 16; // a2
+      } else if (isdigit(c)) {
+        estado = 17; // b2
+      } else if (c == 'm') {
+        estado = 18; // d2
+      } else if (c == 'v') {
+        estado = 19; // e2
+      } else if (c == 'i') {
+        estado = 20; // f2
+      } else if (c == 'c') {
+        estado = 21; // g2
+      } else if (c == 't') {
+        estado = 22; // j2
+      } else if (c == 'e') {
+        estado = 23; // k2
+      } else if (c == 'w') {
+        estado = 24; // m2
+      } else if (c == 'd') {
+        estado = 25; // n2
+      } else if (c == 'f') {
+        estado = 26; // o2
+      } else if (isalpha(c) || c == '_') {
+        estado = 4; // u2
+      }
+      break;
     }
+    case 1: {
+      Token *token = (Token *)malloc(sizeof(Token));
+
+      token->type = TOKEN_OPERATOR;
+      token->operador = Operator::PARDIR;
+      token->line = state.line;
+      token->column = state.column;
+
+      return token;
+    }
+    default: {
+      Token *token = (Token *)malloc(sizeof(Token));
+      token->type = TOKEN_ERROR;
+      token->error = ErrorKind::UNKNOWN_TOKEN;
+      return token;
+    }
+    }
+  }
 }
 
-optional<ReturnToken> diagramIdentifierAndKeyword() {
+int lexer() {
+  FILE *input = fopen("file.txt", "r");
+  if (!input) {
+    printf("Erro ao abrir o arquivo.\n");
+    return 1;
+  }
 
-    int state = 0;
-    string lexeme = "";
-    char c;
+  ReadFile(input);
 
-    while (true) {
-        
-        switch (state) {
-            case 0:
-                c = getNextChar();
-                if (c == 'm') {
-                    lexeme += c;
-                    state = 1;
-                } else if (NotLetterUnderlineOrDigit(c)) {
-                    state = 11;
-                } else return nullopt;
-                break;
+  Token *token;
+  do {
+    token = getNextToken();
+    printf("<%d, %s>\n", token->type, token->value);
+  } while (token->type != TOKEN_EOF && token->type != TOKEN_ERROR);
 
-            case 1:
-                c = getNextChar();
-                if (c == 'a') {
-                    lexeme += c;
-                    state = 2;
-                } else if (NotLetterUnderlineOrDigit(c)) {
-                    state = 11;
-                } else return nullopt;
-                break;
+  fclose(input);
 
-            case 2:
-                c = getNextChar();
-                if (c == 'i') {
-                    lexeme += c;
-                    state = 3;
-                }else if (NotLetterUnderlineOrDigit(c)) {
-                    state = 11;
-                } else return nullopt;
-                break;
-
-            case 3:
-                c = getNextChar();
-                if (c == 'n') {
-                    lexeme += c;
-                    state = 4;
-                } else if (NotLetterUnderlineOrDigit(c)) {
-                    state = 11; // diagrama ID
-                }  else return nullopt;
-                break;
-
-            case 4: // d5
-                c = getNextChar();
-                if (NotLetterUnderlineOrDigit(c)) {
-                    state = 5; 
-                    
-                } else {
-                    lexeme += c;
-                    state = 10; // diagrama 
-                }
-                break;
-            
-                case 5:
-        }
-    }
-}
-
-void lexer() {
-
-    while(ini < buffer.length()) {
-        buffer = getToBuffer();
-        optional<ReturnToken> token = diagramIdentifierAndKeyword();
-        if (token.has_value()) {
-            cout << *token << endl;
-        }
-
-    }
-
+  return 0;
 }
 
 int main() {
-    try {
-        code = PassFileToString();
-        lexer();
-    } catch (const exception& e) {
-        cout << e.what() << endl;
-    }
-
+  try {
+    lexer();
+  } catch (const exception &e) {
+    cout << e.what() << endl;
+  }
 }
--- new/main.h
+++ new/main.h
@@ -1,22 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdlib.h>
 
-class ReturnToken {
-public:
-    string type;
-    string attributeValue;
-    int line;
-    int column;
-
-    ReturnToken(string t, string aV, int l, int c) :
-        type(t), attributeValue(aV), line(l), column(c) {}
-};
-
-ostream& operator<<(ostream& os, const ReturnToken& t);
-bool LetterUnderlineOrDigit(char c); 
-void lexer();
-optional<ReturnToken> diagramVoidWithIdentifier();
-optional<ReturnToken> diagramMainWithIdentifier();
+#define BUFFER_SIZE 4096
+#define MAX_LEXEME_LENGTH 50
 
 enum TokenType {
   TOKEN_ID,
@@ -29,62 +14,56 @@ enum TokenType {
   TOKEN_RELOP,
 };
 
-enum class Relop {
-    GT,
-    LT,
-    LE,
-    EQ,
-    NE,
-    GE
-};
- 
-enum class Operator {
-    SUM,
-    SUB,
-    MULT,
-    DIV,
-    EXP,
-    PARESQ,
-    PARDIR
-};
- 
-enum class Type {
-    CHAR,
-    INT,
-    FLOAT
-};
- 
+enum class Relop { GT, LT, LE, EQ, NE, GE };
+
+enum class Operator { SUM, SUB, MULT, DIV, EXP, PARESQ, PARDIR };
+
+enum class Type { CHAR, INT, FLOAT };
+
 enum class Punctuation {
-    ASSIGMENT,
-    MUL_VARS,
-    END_EXP,
-    DECLARATION,
-    APOSTROPHE,
-    DOT
-};
- 
-enum class Keyword {
-    IF,
-    THEN,
-    ELSE,
-    WHILE,
-    DO,
-    MAIN,
-    BEGIN,
-    END,
-    REPEAT,
-    UNTIL,
-    VOID
-};
- 
-enum class ErrorKind {
-    UNDEFINED = -1,
-    UNCLOSED_COMMENT,
-    INVALID_TOKEN_AFTER_EXCLAMATION,
-    FRACTION_ENDED_WITH_A_DOT,
-    ENDED_WITH_E_EXPOENT,
-    ENDED_AFTER_EXPOENT_SIGN,
-    UNKNOWN_TOKEN
+  ASSIGMENT,
+  MUL_VARS,
+  END_EXP,
+  DECLARATION,
+  APOSTROPHE,
+  DOT
 };
 
- 
+enum class Keyword {
+  IF,
+  THEN,
+  ELSE,
+  WHILE,
+  DO,
+  MAIN,
+  BEGIN,
+  END,
+  REPEAT,
+  UNTIL,
+  VOID
+};
+
+enum class ErrorKind {
+  UNDEFINED = -1,
+  UNCLOSED_COMMENT,
+  INVALID_TOKEN_AFTER_EXCLAMATION,
+  FRACTION_ENDED_WITH_A_DOT,
+  ENDED_WITH_E_EXPOENT,
+  ENDED_AFTER_EXPOENT_SIGN,
+  UNKNOWN_TOKEN
+};
+
+typedef struct {
+  TokenType type;
+  char value[MAX_LEXEME_LENGTH];
+  union {
+    Operator operador;
+    Keyword keyword;
+    Type numberType;
+    Punctuation pontuacao;
+    Relop relop;
+    ErrorKind error;
+  };
+  int line;
+  int column;
+} Token;
